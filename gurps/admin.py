1d9fd52fe0dd034a61278ef0393442149a551b4b
MarcosDEV <marcosvrsdevmail@gmail.com>
2024-11-11 18:34:20 -0300

    Mudança na abordagem do Model.

--- gurps/admin.py
+++ gurps/admin.py
@@ -1,11 +1,55 @@
 from django.contrib import admin
-from .models import RegisterUser, FichaPersonagem  # Importe o modelo RegisterUser
+from .models import RegisterUser, Campanha, FichaPersonagem
 
 
 @admin.register(RegisterUser)
 class RegisterUserAdmin(admin.ModelAdmin):
     list_display = ("username", "email", "created_date", "is_staff")
     search_fields = ("username", "email")
+    fieldsets = (
+        (
+            None,
+            {
+                "fields": (
+                    "username",
+                    "email",
+                    "created_date",
+                    "is_staff",
+                    "groups",
+                    "user_permissions",
+                )
+            },
+        ),
+    )
+
+
+@admin.register(Campanha)
+class CampanhaAdmin(admin.ModelAdmin):
+    list_display = (
+        "nome",
+        "xp_inicial",
+        "xp_acumulado",
+        "pontos_de_desvantagens",
+        "tl",
+    )
+    search_fields = ("nome",)
+    fieldsets = (
+        (
+            None,
+            {
+                "fields": (
+                    "nome",
+                    "xp_inicial",
+                    "pontos_de_desvantagens",
+                    "xp_acumulado",
+                    "tl",
+                    "fichas_players",
+                    "descricao",
+                    "regras",
+                )
+            },
+        ),
+    )
 
 
 @admin.register(FichaPersonagem)
@@ -15,9 +59,35 @@ class FichaPersonagemAdmin(admin.ModelAdmin):
         "campanha",
         "nome_personagem",
         "total_de_pontos",
-    )  # Campos principais para exibição
-    search_fields = (
-        "nome_jogador",
-        "campanha",
-        "nome_personagem",
-    )  # Campos para a barra de pesquisa
+        "pontos_de_experiencia_guardados",
+    )
+    search_fields = ("nome_jogador__username", "campanha__nome", "nome_personagem")
+    fieldsets = (
+        (
+            None,
+            {
+                "fields": (
+                    "nome_jogador",
+                    "campanha",
+                    "nome_personagem",
+                    "idade",
+                    "aparencia",
+                    "altura",
+                    "peso",
+                    "tl",
+                    "total_de_pontos",
+                    "pontos_de_experiencia_guardados",
+                    "pontos_de_vantagens",
+                    "pontos_de_desvantagens",
+                    "pontos_de_pericias",
+                    "pontos_de_atributos",
+                    "attributes",
+                    "subattributes",
+                    "advantages",
+                    "disadvantages",
+                    "skills",
+                    "equipments",
+                )
+            },
+        ),
+    )
--- gurps/models.py
+++ gurps/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+
 from django.utils import timezone
 from django.contrib.auth.models import AbstractUser
 from django.conf import settings
@@ -29,11 +30,14 @@ class RegisterUser(AbstractUser):
 
 class Campanha(models.Model):
     nome = models.CharField(max_length=100)
-    xp_inicial = ...
-    xp_acumulado = ...
-    tl = ...
+    xp_inicial = models.IntegerField(default=0)
+    pontos_de_desvantagens = models.IntegerField(default=0)
+    xp_acumulado = models.IntegerField(default=0)
+    tl = models.IntegerField(default=0)
+    fichas_players = models.JSONField(blank=True, null=True)
 
     descricao = models.TextField(blank=True, null=True)
+    regras = models.TextField(blank=True, null=True)
 
     def __str__(self):
         return self.nome
@@ -48,12 +52,16 @@ class FichaPersonagem(models.Model):
         settings.AUTH_USER_MODEL,  # Refere-se ao modelo de usuário personalizado
         on_delete=models.CASCADE,
         editable=False,  # Impede edição direta do campo
+        null=True,
+        blank=True,
     )
 
     campanha = models.ForeignKey(
         Campanha,
         on_delete=models.CASCADE,
         editable=False,  # O backend definirá a campanha automaticamente
+        null=True,
+        blank=True,
     )
 
     # Outros campos da ficha do personagem
@@ -77,36 +85,13 @@ class FichaPersonagem(models.Model):
     pontos_de_pericias = models.IntegerField(default=0)
     pontos_de_atributos = models.IntegerField(default=0)
 
-    # Exemplo de atributos do personagem
-    st = models.IntegerField(default=10)  # Força base
-    xpst = models.IntegerField(default=0)  # Experiência em força
-
-    dx = models.IntegerField(default=10)  # Destreza base
-    xpdx = models.IntegerField(default=0)  # Experiência em destreza
-
-    iq = models.IntegerField(default=10)  # Inteligência base
-    xpiq = models.IntegerField(default=0)  # Experiência em inteligência
-
-    ht = models.IntegerField(default=10)  # Saúde base
-    xpht = models.IntegerField(default=0)  # Experiência em saúde
-
-    damage = models.CharField(max_length=50, blank=True, null=True)
-    basic_lift_lbs = models.DecimalField(
-        max_digits=5, decimal_places=2, blank=True, null=True
-    )
-    hp = models.IntegerField(default=10)  # Pontos de vida
-    will = models.IntegerField(default=10)  # Vontade
-    perception = models.IntegerField(default=10)  # Percepção
-    fp = models.IntegerField(default=10)  # Pontos de fadiga
-    basic_speed = models.DecimalField(
-        max_digits=4, decimal_places=2, blank=True, null=True
-    )
-    basic_move = models.IntegerField(default=5)  # Movimento básico
-
-    advantages = models.TextField(blank=True, null=True)
-    disadvantages = models.TextField(blank=True, null=True)
-    skills = models.TextField(blank=True, null=True)
-    equipments = models.TextField(blank=True, null=True)
+    # Ficha
+    attributes = models.JSONField(blank=True, null=True)
+    subattributes = models.JSONField(blank=True, null=True)
+    advantages = models.JSONField(blank=True, null=True)
+    disadvantages = models.JSONField(blank=True, null=True)
+    skills = models.JSONField(blank=True, null=True)
+    equipments = models.JSONField(blank=True, null=True)
 
     def __str__(self):
         return f"{self.nome_personagem} - {self.nome_jogador}"
