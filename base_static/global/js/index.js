d8e3dbd87e6969d5ceebe80e739a49db8b805064
MarcosDEV <marcosvrsdevmail@gmail.com>
2024-11-13 00:16:42 -0300

    Correção no JS para retirar o menu quando clicar em sair

--- base_static/global/js/index.js
+++ base_static/global/js/index.js
@@ -2,6 +2,12 @@ document.addEventListener('DOMContentLoaded', () => {
     // Seleciona todos os links do menu
     document.querySelectorAll('.li-menu a').forEach(link => {
         link.addEventListener('click', function (event) {
+            // Verifica se o link é o de logout
+            if (this.href.includes('logout')) {
+                // Oculta o menu antes de redirecionar
+                document.querySelector('.menu-container').style.display = 'none';
+            }
+
             event.preventDefault(); // Evita o carregamento normal
 
             const url = this.href; // Obtém a URL do link clicado
