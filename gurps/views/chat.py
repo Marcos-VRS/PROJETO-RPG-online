5e7bae61c378965a8b95e700a1fdddcb1de264ea
Marcos <marcosvrsdevmail@gmail.com>
2025-01-20 11:28:32 -0300

    fix(Chat): Mensagens agora aparecem no chat
    As mensagens não estavam aparecendo no chat devido a um erro na hora de chamar as variáveis no html. Problema corrigido

--- gurps/views/chat.py
+++ gurps/views/chat.py
@@ -1,6 +1,10 @@
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render, redirect, get_object_or_404
+from gurps.models import Grupo_Chat
 
 
+@login_required(login_url="gurps:login")
 def chat_view(request):
-    return render(request, "global/chat.html")
+    grupo_chat = get_object_or_404(Grupo_Chat, group_name="Chat publico teste")
+    mensagens_chat = grupo_chat.mensagens_chat.all()[:30]
+    return render(request, "global/chat.html", {"mensagens_chat": mensagens_chat})
