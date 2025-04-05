8c547d56d39fa991801a0e2f7ed9397fac9ccc83
Marcos <marcosvrsdevmail@gmail.com>
2025-04-05 05:18:31 -0300

    feat(Global): Implementa os filtros de busca nos menus e listas

---
+++ base_static/global/js/menu/filter_campaign_list.js
@@ -0,0 +1,18 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const input = document.getElementById("search-campanha");
+    const campanhas = document.querySelectorAll(".item-lista");
+
+    input.addEventListener("input", function () {
+        const filtro = input.value.toLowerCase();
+
+        campanhas.forEach(campanha => {
+            const titulo = campanha.querySelector(".campanha-nome").textContent.toLowerCase();
+
+            if (titulo.includes(filtro)) {
+                campanha.style.display = ""; // Mostra o item
+            } else {
+                campanha.style.display = "none"; // Esconde completamente
+            }
+        });
+    });
+});
